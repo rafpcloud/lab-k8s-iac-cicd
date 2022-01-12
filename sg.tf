6a2b125db129403b92c5cf8c98f5662e3aff5c70
Pacheco <rafpcloud@gmail.com>
2022-01-12 11:27:52 -0300

    libera local

--- sg.tf
+++ sg.tf
@@ -12,6 +12,15 @@ resource "aws_security_group" "sgweb" {
 
   }
 
+  ingress {
+    description = "Libera tudo apenas rede local"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.168.0.0/16"]
+
+  }
+
 egress {
     from_port        = 0
     to_port          = 0
